2d6c26d13892d19cc923b110b6f58c9504fc2678
influ <26268158+othman316@users.noreply.github.com>
2023-05-02 13:34:32 +0300

    first commit

---
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import Button from '@/components/ui/Button'
+import { authOptions } from '@/lib/auth'
+import { getServerSession } from 'next-auth'
+import { FC } from 'react'
+
+interface pageProps {}
+
+const page = async ({}) => {
+  const session = await getServerSession(authOptions)
+  return <>{JSON.stringify(session)}</>
+}
+
+export default page
